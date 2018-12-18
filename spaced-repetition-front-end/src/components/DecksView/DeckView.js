fb301bb4d477d9a75602f64b19f581778490a4ec
gooseandmegander <meganawilliamson@gmail.com>
2018-12-18 12:05:10 -0800

    Resolves warnings in DeckView

--- spaced-repetition-front-end/src/components/DecksView/DeckView.js
+++ spaced-repetition-front-end/src/components/DecksView/DeckView.js
@@ -12,69 +12,75 @@ import handleCardSnippets from '../../snippets';
 class DeckView extends Component {
   state = {
     addNewCard: false,
-    // deckArr: [],
-    deck: false
+    deck: false,
   };
 
   componentDidMount = () => {
-    const selectedDeckID = this.props.match.params.deckId;
+    const { props: { match: { params: { deckId } } } } = this.props;
+    const selectedDeckID = deckId;
     const { decks, history } = this.props;
-
     let match = false;
-    for (let i = 0; i < decks.length; i++) {
+    for (let i = 0; i < decks.length; i += 1) {
       if (decks[i].id === Number(selectedDeckID)) {
-        console.log('match');
         match = decks[i];
       }
     }
-
     if (!match) history.push('/dashboard/decks');
     this.setState({ deck: match });
   }
 
-
   handleAddCard = () => {
-    this.setState({ addNewCard: !this.state.addNewCard });
+    this.setState(prevState => ({ addNewCard: !prevState.addNewCard }));
   }
 
   handleDeckData = () => {
     const { decks } = this.props;
     const deckData = decks.map(deck => ({ id: deck.id, name: deck.name }));
-
     return deckData;
   }
 
   render() {
-    const { today, decks } = this.props;
+    const { today, decks, match: { params: { deckId } } } = this.props;
     const { addNewCard, deck } = this.state;
-    const selectedDeckID = this.props.match.params.deckId;
+    const selectedDeckID = deckId;
     return (
-      deck &&
-      <DeckViewContainer>
-        <Header>
-          <CardListTools addNewCard={this.handleAddCard} />
-          <Deck deck={deck} today={today} disableDelete disableTraining />
-        </Header>
-
-
-        <CardsContainer>
-
-          {addNewCard && <AddCard grabDeckInfo={this.handleDeckData} toggleAddCard={this.handleAddCard} deckID={selectedDeckID} />}
-
-          {deck.cards.map((card) => {
-            const formattedCard = handleCardSnippets(card);
-            return <Card key={`${card.id} ${card.title}`} card={formattedCard} deckName={deck.name} decks={decks} />;
-          })}
-
-        </CardsContainer>
-      </DeckViewContainer>
+      deck
+      && (
+        <DeckViewContainer>
+          <Header>
+            <CardListTools addNewCard={this.handleAddCard} />
+            <Deck deck={deck} today={today} disableDelete disableTraining />
+          </Header>
+          <CardsContainer>
+            {addNewCard && (
+              <AddCard
+                grabDeckInfo={this.handleDeckData}
+                toggleAddCard={this.handleAddCard}
+                deckID={selectedDeckID}
+              />
+            )}
+            {deck.cards.map((card) => {
+              const formattedCard = handleCardSnippets(card);
+              return (
+                <Card
+                  key={`${card.id} ${card.title}`}
+                  card={formattedCard}
+                  deckName={deck.name}
+                  decks={decks}
+                />
+              );
+            })}
+          </CardsContainer>
+        </DeckViewContainer>
+      )
+      // What are we returning if there isn't a deck?
     );
   }
 }
 
 export default withRouter(DeckView);
 
-// styled
+// styles
 
 const DeckViewContainer = styled.div`
 width: 100%;
@@ -86,28 +92,26 @@ justify-content: center;
 background: ${props => props.theme.dark.bodyBackground};
 overflow: auto;
 padding-bottom: 5%;
-  @media (max-width: 500px) {
-    margin-left: 0;
-    margin-top: 65px;
-  }
+
+@media (max-width: 500px) {
+  margin-left: 0;
+  margin-top: 65px;
+}
 `;
 
 const Header = styled.div`
-  display: flex;
-  flex-direction:column;
-  width: 100%;
-  /* justify-content: center; */
-  align-items: center;
+display: flex;
+flex-direction:column;
+width: 100%;
+align-items: center;
 `;
-
 
 const CardsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+width: 100%;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
 `;
-
 
 DeckView.propTypes = {
   decks: PropTypes.instanceOf(Object).isRequired,
