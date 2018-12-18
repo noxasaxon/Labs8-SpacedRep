d24c1fe7f22d5fe3d8c6993338161f86b91d9a77
gooseandmegander <meganawilliamson@gmail.com>
2018-12-18 08:34:54 -0800

    resolving warnings in CardInputs

--- spaced-repetition-front-end/src/components/SharedComponents/CardInputs.js
+++ spaced-repetition-front-end/src/components/SharedComponents/CardInputs.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// save button no longer needed (it updates dynamically)
-// should implement a delete button to remove a card
-
 class CardInputs extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       title: '',
       question: '',
       answer: '',
-      language: 'Plain Text',
     };
   }
 
@@ -27,19 +22,23 @@ class CardInputs extends React.Component {
   }
 
   render() {
-    const { state, props } = this;
+    const { title, question, answer } = this.state;
+    const { i, removeCard } = this.props;
     return (
       <Container>
         <CardInfo>
           <Header>
-            {`Card #${props.i + 1}`}
+            {`Card #${i + 1}`}
             <Caption> Supports code snippets too, just surround code with 3 backticks ``` </Caption>
-            {props.i !== 0 && <Cancel type="button" onClick={() => props.removeCard(props.i)}>x</Cancel>}
+            {i !== 0 && <Cancel type="button" onClick={() => removeCard(i)}>x</Cancel>}
           </Header>
           {/* Title and Language should be labels instead of p */}
-          <DescriptionLine> <p>Title</p> <p>Language</p> </DescriptionLine>
+          <DescriptionLine>
+            <p>Title</p>
+            <p>Language</p>
+          </DescriptionLine>
           <TopRow>
-            <input type="text" value={state.title} name="title" onChange={this.handleChange} placeholder="Title" required />
+            <input type="text" value={title} name="title" onChange={this.handleChange} placeholder="Title" required />
             <Dropdown name="language" onChange={this.handleChange}>
               <DropdownOption value="Plain Text">Plain Text</DropdownOption>
               <DropdownOption value="JavaScript">JavaScript</DropdownOption>
@@ -49,17 +48,14 @@ class CardInputs extends React.Component {
               <DropdownOption value="C++">C++</DropdownOption>
             </Dropdown>
           </TopRow>
-
           <DescriptionLine>
             <Description>Question </Description>
           </DescriptionLine>
-          <TextArea type="text" value={state.question} name="question" onChange={this.handleChange} placeholder="Question" required />
-
+          <TextArea type="text" value={question} name="question" onChange={this.handleChange} placeholder="Question" required />
           <DescriptionLine>
             <Description>Answer </Description>
           </DescriptionLine>
-          <TextArea type="text" value={state.answer} name="answer" onChange={this.handleChange} placeholder="Answer" required />
-
+          <TextArea type="text" value={answer} name="answer" onChange={this.handleChange} placeholder="Answer" required />
         </CardInfo>
       </Container>
     );
@@ -69,102 +65,93 @@ class CardInputs extends React.Component {
 export default CardInputs;
 
 const Container = styled.div`
-  width: 100%;
-  margin: 6px 0px 6px 0px;
-`
+width: 100%;
+margin: 6px 0px 6px 0px;
+`;
 
 const Header = styled.h3`
-  display: flex;
-  justify-content:space-between;
-  align-items: center;
-  /* align-content:center; */
-  width: 100%;
-  margin-bottom: 12px;
-  font-size: 18px;
-`
+display: flex;
+justify-content:space-between;
+align-items: center;
+width: 100%;
+margin-bottom: 12px;
+font-size: 18px;
+`;
 
 const Cancel = styled.button`
-  border: none;
-  background: none;
-  color: lightgrey;
-  font-weight: bold;
-  font-size: 24px;
-  height: 26px;
-  margin: 0px;
-  color: ${props => props.theme.dark.buttons.negative};
-  &:hover {
-    background: grey;
-  }
-  /* width: 100px; */
+border: none;
+background: none;
+color: lightgrey;
+font-weight: bold;
+font-size: 24px;
+height: 26px;
+margin: 0px;
+color: ${props => props.theme.dark.buttons.negative};
+&:hover {
+  background: grey;
+}
 `;
 
 const DescriptionLine = styled.div`
-  display:flex;
-  justify-content:space-between;
-  font-size: 18px;
-  padding-bottom: 2px;
-`
+display:flex;
+justify-content:space-between;
+font-size: 18px;
+padding-bottom: 2px;
+`;
+
 const Caption = styled.p`
-  font-size: 14px;
-  color: lightgrey;
-`
+font-size: 14px;
+color: lightgrey;
+`;
 
 const TopRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  background: #5e707b;
-  border-radius: 3px;
-  align-items: baseline;
-  justify-content: space-between;
-  box-shadow: none;
+display: flex;
+flex-direction: row;
+width: 100%;
+background: #5e707b;
+border-radius: 3px;
+align-items: baseline;
+justify-content: space-between;
+box-shadow: none;
 
-  input[name="title"] {
-    flex-grow:1;
-  }
+input[name="title"] {
+  flex-grow:1;
+}
 
-  button, select {
-    margin-left: 5px;
-  }
+button, select {
+  margin-left: 5px;
+}
 `;
 
 const CardInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 10px;
-  background: ${props => props.theme.dark.cardBackground};
-  border-radius: 3px;
-  box-shadow: none;
-
-  input[type="text"] {
-    /* width: 100%; */
-  }
-
+display: flex;
+flex-direction: column;
+width: 100%;
+padding: 10px;
+background: ${props => props.theme.dark.cardBackground};
+border-radius: 3px;
+box-shadow: none;
 `;
 
 const Description = styled.p`
-  font-size: 18px;
-  padding-right: 10px;
-`
-
-
+font-size: 18px;
+padding-right: 10px;
+`;
 
 const Dropdown = styled.select`
-  border-radius: 3px;
-  background-color: lightgray;
-  border: none;
-  height: 50px;
+border-radius: 3px;
+background-color: lightgray;
+border: none;
+height: 50px;
 `;
 
 const DropdownOption = styled.option`
-  /* background: darkgrey; */
-  background: ${props => props.theme.dark.main};
-  color: white;
-`
+background: ${props => props.theme.dark.main};
+color: white;
+`;
 
 const TextArea = styled.textarea`
-    height: 75px;
-    padding: 15px;
-    resize: vertical;
-`
+height: 75px;
+padding: 15px;
+resize: vertical;
+`;
