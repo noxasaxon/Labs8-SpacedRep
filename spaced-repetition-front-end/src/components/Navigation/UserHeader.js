03037c05bf4edddae47eb2d0d55965873f399421
gooseandmegander <meganawilliamson@gmail.com>
2018-12-18 09:44:29 -0800

    Resolves warning issues in Nav
    Ended up disabling object-curly-newline because it causes issue with body's indentation. Need to figure out if this is Prettier issue and how to fix.

--- spaced-repetition-front-end/src/components/Navigation/UserHeader.js
+++ spaced-repetition-front-end/src/components/Navigation/UserHeader.js
@@ -30,12 +30,6 @@ const UserHeader = ({ auth }, props) => {
 
 export default UserHeader;
 
-UserHeader.propTypes = {
-  auth: PropTypes.shape({
-    isAuthenticated: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
 // styles
 
 const Container = styled.div`
@@ -52,16 +46,15 @@ align-items: center;
 justify-content: space-between;
 background: ${props => props.theme.dark.main};
 border-bottom: 1px solid white;
-/* margin: 0 auto;  //needed to fix header going off center when zoom out?*/
 `;
 
 const AppName = styled(Link)`
 align-self: center;
 
-  h1 {
-    font-family: 'Comfortaa', cursive;
-    font-size: 26px;
-  }
+h1 {
+  font-family: 'Comfortaa', cursive;
+  font-size: 26px;
+}
 `;
 
 const LinkStyled = styled.button`
@@ -75,3 +68,9 @@ background: none;
 border-radius: 3px;
 border: 1px solid lightseagreen;
 `;
+
+UserHeader.propTypes = {
+  auth: PropTypes.shape({
+    isAuthenticated: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- spaced-repetition-front-end/src/components/Navigation/VisitorHeader.js
+++ spaced-repetition-front-end/src/components/Navigation/VisitorHeader.js
@@ -1,5 +1,8 @@
+/* eslint no-confusing-arrow: ["off"] */
+// disabling no-confusing-arrow to use styled-components syntax
+
 import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Nav from './Nav';
@@ -35,7 +38,17 @@ class VisitorHeader extends Component {
         <Nav toggle={toggle} login={this.login} logout={this.logout} isLoggedIn={isAuthenticated} />
         <BurgerGroup isLoggedIn={isAuthenticated()}>
           {isAuthenticated() ? <a href="/dashboard">Dashboard</a> : null}
-          {toggle ? <CloseIcon type="button" onClick={this.toggleNav}><i class="fas fa-times fa-2x"></i></CloseIcon> : <BurgerIcon type="button" onClick={this.toggleNav}><i className="fas fa-bars fa-2x" /></BurgerIcon>}
+          {toggle
+            ? (
+              <CloseIcon type="button" onClick={this.toggleNav}>
+                <i className="fas fa-times fa-2x" />
+              </CloseIcon>
+            ) : (
+              <BurgerIcon type="button" onClick={this.toggleNav}>
+                <i className="fas fa-bars fa-2x" />
+              </BurgerIcon>
+            )
+          }
         </BurgerGroup>
       </Container>
     );
@@ -44,62 +57,55 @@ class VisitorHeader extends Component {
 
 export default VisitorHeader;
 
-VisitorHeader.propTypes = {
-  auth: PropTypes.shape({
-    isAuthenticated: PropTypes.func.isRequired,
-  }).isRequired,
-};
-
 // styles
+
 const Container = styled.div`
-    padding: 0 2%;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    width: 100%;
-    max-width: 1500px;
-    align-items: center;
-    height: 55px;
-    display: flex;
-    justify-content: space-between;
-    border-bottom: 1px solid white;
-    background: ${props => props.theme.dark.main};
-  /* margin: 0 auto;  //needed to fix header going off center when zoom out?*/
+padding: 0 2%;
+position: fixed;
+z-index: 1;
+top: 0;
+left: 0;
+width: 100%;
+max-width: 1500px;
+align-items: center;
+height: 55px;
+display: flex;
+justify-content: space-between;
+border-bottom: 1px solid white;
+background: ${props => props.theme.dark.main};
 
-  @media (max-width: 500px) {
-    height: ${props => props.isLoggedIn ? '90px' : '55px'};
-    flex-direction: ${props => props.isLoggedIn ? 'column' : 'row'};
-    justify-content: center;
-  }
+@media (max-width: 500px) {
+height: ${props => props.isLoggedIn ? '90px' : '55px'};
+flex-direction: ${props => props.isLoggedIn ? 'column' : 'row'};
+justify-content: center;
+}
 
-  a {
-    font-size: 16px;
+a {
+font-size: 16px;
 
-    &:hover {
-      text-decoration: none;
-    }
-  }
+&:hover {
+  text-decoration: none;
+}
+}
 `;
 
 const AppName = styled(Link)`
-  align-self: center;
-  
-  &:hover {
-    text-decoration: none;
-  }
-      
-  h1 {
-    font-family: 'Comfortaa', cursive;
-    font-size: 38px;
-    font-weight: bold;
-  }
+align-self: center;
+
+&:hover {
+  text-decoration: none;
+}
+    
+h1 {
+  font-family: 'Comfortaa', cursive;
+  font-size: 38px;
+  font-weight: bold;
+}
 `;
 
 const BurgerGroup = styled.div`
 width: 100%;
 max-width: 150px;
-// display: flex;
 justify-content: ${props => props.isLoggedIn ? 'space-between' : 'flex-end'};
 align-items: center;
 display: none;
@@ -123,62 +129,16 @@ a {
 
 const BurgerIcon = styled.div`
 display: none;
-    
-    @media (max-width: 900px) {
-      display: inline-block;
-    }
+
+@media (max-width: 900px) {
+  display: inline-block;
+}
 `;
 
 const CloseIcon = styled.div``;
 
-    // const Logo = styled.img`
-    //   width: 75%;
-    //   max-width: 200px;
-    //   min-width: 100px;
-    // `;
-
-    // const LinkStyled = styled.button`
-    //   font-size: 14px;
-    //   height: 25px;
-    //   width: 90px;
-    //   cursor: pointer;
-    //   border-radius: 3px;
-    //   color: lightseagreen;
-    //   margin-left: 5%;
-    //   background: none;
-    //   border: 1px solid lightseagreen;
-
-//   // @media (max-width: 400px) {
-//   //   margin: 0 0 15px 0;
-//   //   border: none;
-//   //   color: white;
-//   // }
-// `;
-
-// const VisitorsNav = styled.div`
-// align-items: baseline;
-// display: flex;
-// width: 60%;
-// // max-width: 50%;
-// justify-content: space-between;
-
-// // @media (max-width: 1000px) {
-// //   // max-width: 50%;
-// // }
-
-// // @media (max-width: 900px) {
-// //   // width: 100%;
-// //   // max-width: 100%;
-
-// //   }
-
-// //   @media (max-width: 400px) {
-// //     flex-direction: column;
-// //     height: 100%;
-// //     align-items: center;
-// //   }
-// `;
-
-// const UsersNav = styled(VisitorsNav)`
-//   justify-content: flex-end;
-// `;
+VisitorHeader.propTypes = {
+  auth: PropTypes.shape({
+    isAuthenticated: PropTypes.func.isRequired,
+  }).isRequired,
+};
